08ddc91eab011c0c087dcf6ee152755a04a9d983
kakkadhaval <dhavalkakkad2201@gmail.com>
2026-08-01 19:26:33 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = process.env.GH_PAGES_REPO || "";
+
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  basePath: repoName ? `/${repoName}` : "",
+  assetPrefix: repoName ? `/${repoName}/` : "",
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
   },
 };
